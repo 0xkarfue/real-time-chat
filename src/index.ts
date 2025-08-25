0c0bcd8e3205ca784eb13cfa2ae368663f828121
0xkarfue <0xkarfue@gmail.com>
2025-08-25 13:02:35 +0530

    basic chat app

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,64 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+    roomId: string,
+    // socket: WebSocket[]
+    socket: WebSocket
+}
+
+let allSocket: User[] = [] 
+
 wss.on("connection", (socket) => {
-    //  6: 00    
-    console.log("user connected!");
-    socket.send("chat started!")
-    socket.on("message", (data) => {
-        console.log(data.toString());
-        wss.clients.forEach((client) => {
-            if(client.readyState === WebSocket.OPEN) {
-                client.send(data);
+    console.log("user connected!")
+    socket.on("message", (msg) => {
+        const parsedMsg = JSON.parse(msg as unknown as string)
+        if(parsedMsg.type == "join") {
+            allSocket.push({
+                socket,
+                roomId: parsedMsg.payload.roomId
+            })
+        }
+        if(parsedMsg.type == "chat") {
+            const currentUserRoom = allSocket.find((x) => x.socket == socket)?.roomId
+
+            for(let i = 0; i < allSocket.length; i++) {
+                if (allSocket[i].roomId == currentUserRoom) {
+                    allSocket[i].socket.send(parsedMsg.payload.message)
+                }
             }
-        })
+        }
     })
-    socket.on("close", () => {
-        console.log("user disconnected!")
-    })
-})  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+ // basic chat app
+
+// import WebSocket, { WebSocketServer } from "ws";
+
+// const wss = new WebSocketServer({ port: 8080 });
+
+// let allSocket = []
+
+// wss.on("connection", (socket) => {
+//     console.log("user connected!")
+//     allSocket.push(socket)
+//     socket.on("message", (message) => {
+//         console.log("message   =  " + message.toString())
+//         allSocket.forEach((element) => {
+//             element.send("Message from server  " + message.toString())
+//         })
+//     }) 
+// })
+
+
